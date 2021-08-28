4391ef92f4a210eb49b7819e91122284018a8681
Akhil Kala <akhilkala@pop-os.localdomain>
2021-08-28 09:51:07 +0530

    home page sidebar added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,10 +8,20 @@ export default function Home() {
         <div className="home">
             <aside>
                 <div className="top">
-                    
+                <h2>
+                    Medical <br/> Assistant
+                </h2>
                 </div>
-                <div className="logout" onClick={auth.logout} > 
+                <div className="bottom">
+                <button> 
+                  I am a Merchant
+                </button>
+                <button > 
+                  I am a Doctor
+                </button>
+                <button className="logout" onClick={auth.logout} > 
                     <i class="fas fa-sign-out-alt"></i> Logout
+                </button>
                 </div>
             </aside>
             <main>
